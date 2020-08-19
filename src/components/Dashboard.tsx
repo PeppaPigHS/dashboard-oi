5a8c25962227ea0c24036fb304cc13dd0e72d3d6
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-08-19 18:24:51 +0700

    Create landing page

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,67 @@
 import React from 'react'
+import Link from 'next/link'
+
+import { Flex, Text, Button, Box, Divider } from '@chakra-ui/core'
+import { FaArrowRight, FaGithub } from 'react-icons/fa'
+
+import { useUser } from 'components/UserContext'
 
 export const Dashboard = () => {
-  return <React.Fragment></React.Fragment>
+  const { user } = useUser()
+
+  return (
+    <Flex flexGrow={1} direction="column" justify="center">
+      <Box w="full" pb="12" pt="3" mx="auto" maxW="1024px" px={6}>
+        <Box maxW="760px" mx="auto" textAlign="center">
+          <Text
+            fontSize={{ base: '2.25rem', md: '3rem', lg: '3.75rem' }}
+            letterSpacing="tight"
+            mb="16px"
+            lineHeight="1.2"
+          >
+            Checklist built for
+            <br />
+            <Box as="span" color="teal.500">
+              {' '}
+              competitive programming enthusiasts
+            </Box>
+          </Text>
+
+          <Text opacity={0.7} fontSize={{ base: 'lg', lg: 'xl' }} mt="6">
+            DashboardOI is a checklist for keep tracking your progress on
+            solving OI problems
+          </Text>
+
+          <Divider></Divider>
+
+          <Flex
+            mt="10"
+            justify="center"
+            direction={{ base: 'column', sm: 'row' }}
+          >
+            <Button
+              mr={[0, 4]}
+              mb={[4, 0]}
+              px="40px"
+              fontSize="1.2rem"
+              variantColor="teal"
+              leftIcon={FaArrowRight}
+            >
+              <Link href={user.user ? '/checklist' : '/login'}>
+                <a>Get Started</a>
+              </Link>
+            </Button>
+            <Button px="40px" fontSize="1.2rem" leftIcon={FaGithub}>
+              <a
+                href="https://github.com/PeppaPigHS/dashboard-oi"
+                target="__blank"
+              >
+                GitHub
+              </a>
+            </Button>
+          </Flex>
+        </Box>
+      </Box>
+    </Flex>
+  )
 }
