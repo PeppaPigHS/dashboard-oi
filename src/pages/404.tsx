bd6fc3d5bc8b89193ffa7da94d931d2e509417bf
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-08-19 18:25:31 +0700

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Text, Flex } from '@chakra-ui/core'
+
+const FourZeroFour = () => (
+  <Flex align="center" justify="center" flexGrow={1}>
+    <Text fontSize="3xl" textAlign="center">
+      404 | Not Found
+    </Text>
+  </Flex>
+)
+
+export default FourZeroFour
