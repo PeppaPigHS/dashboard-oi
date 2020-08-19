17e1f1e95b099aee2e53dbacbda918dbdbe5e59d
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-08-19 18:26:38 +0700

    Update layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 import { Flex, Divider } from '@chakra-ui/core'
-import { Nav } from './Nav'
+
+import { Nav } from 'components/Nav'
+import { Footer } from 'components/Footer'
+import { useUser } from 'components/UserContext'
 
 interface IPageLayoutProps {
   hideNav?: boolean
@@ -12,13 +15,21 @@ interface IPageLayoutProps {
 export const PageLayout: React.FunctionComponent<IPageLayoutProps> = (
   props: IPageLayoutProps
 ) => {
+  const { user } = useUser()
+
   return (
-    <Flex direction="column" minHeight="100vh" bg={props.bg}>
+    <Flex
+      direction="column"
+      minHeight="100vh"
+      bg={props.bg}
+      display={user.user === undefined ? 'none' : 'flex'}
+    >
       <Nav></Nav>
       <Divider m={0}></Divider>
       <Flex as="main" flex="auto" direction="column" minH="calc(100vh - 56px)">
         {props.children}
       </Flex>
+      <Footer bg={props.bg} />
     </Flex>
   )
 }
