f70c8a7fca2e7e850c957ac68e5a11c9460c061c
PeppaPigHS <ppeppapig.stuff@gmail.com>
2020-08-19 18:25:51 +0700

    Redirect to home page when logged in

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 import { Flex, Text, Box, Link as ChakraLink } from '@chakra-ui/core'
 
 import { OAuthLogin } from 'components/auth/OAuthLogin'
+import { useUser } from 'components/UserContext'
 
 export const Login = () => {
   const [error, setError] = useState<string>(' ')
 
+  const { user } = useUser()
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (user.user !== null) {
+      router.push('/')
+    }
+  }, [user.user])
+
   return (
     <Flex align="center" justify="center" flexGrow={1} flexDirection="column">
-      <Text />
       <Text fontSize={['4xl', '5xl']} mb={4}>
         Log in
       </Text>
